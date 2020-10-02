c332b158c94f27184b1abd2294004a8d38f4c454
vaibhavmahindru <500067728@stu.upes.ac.in>
2020-10-02 18:18:27 +0530

    Task 1 submitted, LINCHESS added

---
+++ Task 1/LINCHESS/Solution.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class solution {
+    static Scanner in = new Scanner(System.in);
+
+    private static int check() {
+        int num = -1;
+        int count = 100000000;
+
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+
+            arr[i] = in.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+
+            int a = arr[i];
+            int c = 1;
+            while (a <= k) {
+                a = arr[i] * c;
+                c++;
+            }
+            a -= arr[i];
+
+            if (a == k) {
+
+                if (c < count) {
+                    count = c;
+                    num = arr[i];
+                }
+            }
+
+        }
+        return num;
+
+    }
+
+    public static void main(String[] args) {
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = check();
+            System.out.println(a);
+        }
+    }
+}
