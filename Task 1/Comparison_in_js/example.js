ef5b8f20faebd9fbc114d5cffab6bebbdf2e9691
Akrity Pant <akritypant@gmail.com>
2020-10-04 12:34:25 +0545

    js ques and soln added

---
+++ Task 1/Comparison_in_js/example.js
@@ -0,0 +1,14 @@
+let str = "10";
+if (str == 10) {
+  console.log("They are Equal");
+} else {
+  console.log("not equal");
+} //o/p will be 'They are Equal' as data type   is not looked by the ==
+
+// ===
+let str = "10";
+if (str === 10) {
+  console.log("They are equal");
+} else {
+  console.log("not equal");
+} //o/p will be 'not equal as '10' is string and 10 is number
