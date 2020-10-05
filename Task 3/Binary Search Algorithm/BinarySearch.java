a1fcf13b7548bb6eae9e5e7a37b768f054e103a9
Tarun Gupta <the.tarun.gupta.raja@gmail.com>
2020-10-05 14:43:56 +0530

    Update BinarySearch.java

--- Task 3/Binary Search Algorithm/BinarySearch.java
+++ Task 3/Binary Search Algorithm/BinarySearch.java
@@ -3,13 +3,43 @@ package algorithms;
 public class BinarySearch {
 	
 	public static int binarySearchRecursive(int[] arr, int l, int r, int x) {
-		
+		if (r >= l) { 
+        int mid = l + (r - l) / 2; 
+  
+        // If the element is present at the middle 
+        // itself 
+        if (arr[mid] == x) 
+            return mid; 
+  
+        // If element is smaller than mid, then 
+        // it can only be present in left subarray 
+        if (arr[mid] > x) 
+            return binarySearch(arr, l, mid - 1, x); 
+  
+        // Else the element can only be present 
+        // in right subarray 
+        return binarySearch(arr, mid + 1, r, x); 
+    }
 		
 		return -1;
 	}
 	
 	public static int binarySearchIterative(int[] arr) {
-		
+		while (l <= r) { 
+        int m = l + (r - l) / 2; 
+  
+        // Check if x is present at mid 
+        if (arr[m] == x) 
+            return m; 
+  
+        // If x greater, ignore left half 
+        if (arr[m] < x) 
+            l = m + 1; 
+  
+        // If x is smaller, ignore right half 
+        else
+            r = m - 1; 
+    }
 		
 		return -1;
 	}
