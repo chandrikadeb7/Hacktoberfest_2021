ab212d8aaa03090a06c22b69bb457dcb1569329b
VivekWesley <vivekjohnwesley@gmail.com>
2020-10-26 14:51:15 +0530

    Remove Commented Code

--- Task 1/move_common_elements_lefSide_algorithm/move_common_leftside.py
+++ Task 1/move_common_elements_lefSide_algorithm/move_common_leftside.py
@@ -1,28 +1,3 @@
-# def move_common_leftside(arr):
-#     if len(arr) < 1:
-#         return "length issues"
-#     read_stream = len(arr) - 1
-#     write_stream = len(arr) - 1
-
-#     while(read_stream >= 0):
-#         if arr[read_stream] != 0:
-#             arr[write_stream] = arr[read_stream]
-#             write_stream -= 1
-
-#         read_stream -= 1
-    
-#     while(write_stream >= 0):
-#         arr[write_stream] = 0
-#         write_stream -= 1
-    
-#     print("0 shifted left : ", arr)
-
-# arr = [ 5, 6, 10, 0, 60, 0, 0, 90, 0 ]
-
-# print("Original array: ", arr)
-# move_common_leftside(arr)
-
-
 # Algorithm:
 # Move common elements of the array to left side
 # We are moving '0' to <--left which occurs 4 times in the given example.
