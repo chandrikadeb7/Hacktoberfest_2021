d8db41f09f8e61b1117bbb9cecd589b4b80a54d9
kritikakanojia1526 <kritikakanojia790@gmail.com>
2020-10-28 08:32:28 +0530

    Changes made according to the last message

--- Task 3/BFS/BFS.java
+++ Task 3/BFS/BFS.java
@@ -52,7 +52,7 @@ class Graph
   
         g.addEdge(0, 1); 
         g.addEdge(0, 2); 
-        g.addEdge(1, 2); 
+        g.addEdge(1, 1); 
         g.addEdge(2, 0); 
         g.addEdge(2, 3); 
         g.addEdge(3, 3); 
