14d322199be9b066b89b9269d4b9c5b4ef708618
Vivek Wesley <30550226+VivekWesley@users.noreply.github.com>
2020-10-28 16:42:54 +0530

    Update binary_seach_recursive.py

--- Task 3/binary_search_recursive_approach/binary_seach_recursive.py
+++ Task 3/binary_search_recursive_approach/binary_seach_recursive.py
@@ -1,13 +1,5 @@
-#_______________________________________________________________
-# Binary_search using recursion.
-# arr = [ 0, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024 ]
-#        /                |                        \
-#       L                mid                        R
-# 
-# mid = L + ((R-L) // 2)
-# time complexity O(n)
-# Author: Vivek Wesley
-# _______________________________________________________________
+# sample input: 256
+# sample output: 256 found at index:  8
 
 def binary_search_helper(arr, value, L, R):
     mid = L + (( R - L ) // 2)
@@ -29,8 +21,3 @@ def binary_search(arr, value ): # driver function definition
     return binary_search_helper(arr, value, 0, len(arr)-1) #calling recursive function and passing required arguments [Note: L = 0 (first_index_value), R = len(arr)-1] 
 
 print(value, "found at index: ", binary_search(arr, value)) # calling driver function
-
-# sample input: 256
-# sample output: 256 found at index:  8
-
-# __________happy coding__________
