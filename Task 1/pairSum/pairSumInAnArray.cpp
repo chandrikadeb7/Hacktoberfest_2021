459bbe6873535107f2526444f732be05cb45fce3
Himalaya Gupta <guptahimalaya2@gmail.com>
2021-10-01 20:19:51 +0530

    Added Pair Sum in an array Problem and solution with complexity analysis

---
+++ Task 1/pairSum/pairSumInAnArray.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+void printPairs(int arr[], int arr_size, int sum)
+{
+    unordered_set<int> s;
+    for (int i = 0; i < arr_size; i++)
+    {
+        int temp = sum - arr[i];
+ 
+        if (s.find(temp) != s.end())
+            cout << "Pair with given sum "
+                 << sum << " is (" << arr[i] << ","
+                    << temp << ")" << endl;
+ 
+        s.insert(arr[i]);
+    }
+}
+ 
+
+int main()
+{
+    int A[] = { 1, 4, 45, 6, 10, 8 };
+    int n = 16;
+    int arr_size = sizeof(A) / sizeof(A[0]);
+ 
+    printPairs(A, arr_size, n);
+ 
+    return 0;
+}
+
+
+// Time Complexity: O(n). 
+// As the whole array is needed to be traversed only once.
+// Auxiliary Space: O(n). 
+// A hash map has been used to store array elements.
