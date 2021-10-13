1ddae3a731b12f3be7e797ac923dbd83992802f7
SIREESHA2271 <85788597+SIREESHA2271@users.noreply.github.com>
2021-10-13 19:25:02 +0530

    task1

---
+++ Task 1/kth smallest element/solution.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        priority_queue<int> mxhp;
+for(int i=l;i<=r;i++){
+mxhp.push(arr[i]);
+if(mxhp.size()>k)
+mxhp.pop();
+}
+return mxhp.top();
+    }
+};
+ 
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
+        
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
+            
+        int k;
+        cin>>k;
+        Solution ob;
+        cout<<ob.kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+    }
+    return 0;
+}
