c18b45524dc3602e7565fb7c891f70126a6f5daa
carlacentenor <carlacentenor@gmail.com>
2021-10-20 12:36:32 -0500

    add portfolio

---
+++ Task 2/Carlita_portfolio/js/app.js
@@ -0,0 +1,163 @@
+var scroll = new SmoothScroll('a[href*="#"]', {
+  // Selectors
+  ignore: '[data-scroll-ignore]', // Selector for links to ignore (must be a valid CSS selector)
+  header: null, // Selector for fixed headers (must be a valid CSS selector)
+
+  // Speed & Easing
+  speed: 1500, // Integer. How fast to complete the scroll in milliseconds
+  offset: 0, // Integer or Function returning an integer. How far to offset the scrolling anchor location in pixels
+  easing: 'easeInOutCubic', // Easing pattern to use
+  customEasing: function (time) {
+
+    // Function. Custom easing pattern
+    // If this is set to anything other than null, will override the easing option above
+
+    // return <your formulate with time as a multiplier>
+
+    // Example: easeInOut Quad
+    return time < 0.5 ? 2 * time * time : -1 + (4 - 2 * time) * time;
+
+  },
+
+  // Callback API
+  before: function (anchor, toggle) {}, // Callback to run before scroll
+  after: function (anchor, toggle) {} // Callback to run after scroll
+});
+
+
+/**Libreria Particulas */
+
+particlesJS('particles-js',
+  
+  {
+    "particles": {
+      "number": {
+        "value": 80,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 6,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }
+
+);
+
+
+/***Efecto portafolio */
+
+$(window).scroll(function(){
+if($('#navbar-example2').offset().top>500){
+  $('#navbar-example2').addClass('bg-black');
+  $('#navbar-example2').removeClass('transparent');
+}else{
+  $('#navbar-example2').removeClass('bg-black');
+  $('#navbar-example2').addClass('transparent');
+}
+
+})
