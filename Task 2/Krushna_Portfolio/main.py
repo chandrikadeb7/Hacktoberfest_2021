c5dca5d76e714dfa031ac3ccdc1c7055b10b0ebe
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-02-05 18:08:33 -0800

    Added Portfolio inside new branch

---
+++ Task 2/Krushna_Portfolio/main.py
@@ -0,0 +1,9 @@
+from link import open_link
+
+def main():
+    url = "https://aesthetic-peony-a18438.netlify.app"
+    result = open_link(url)
+    return result
+
+if __name__ == "__main__":
+    main()
